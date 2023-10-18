805321e7e3b318aee64f9e6f6b02984d5a983340
yeahright18 <szymonsk312@gmail.com>
2023-10-18 13:39:30 +0200

    commit4

--- TeslaDrive/Data/ApplicationDbContext.cs
+++ TeslaDrive/Data/ApplicationDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using TeslaDrive.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace TeslaDrive.Data
 {
-  public class ApplicationDbContext : DbContext
+  public class ApplicationDbContext : IdentityDbContext
   {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
--- TeslaDrive/Program.cs
+++ TeslaDrive/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using TeslaDrive.Data;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
@@ -23,6 +26,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.MapRazorPages();
 
 app.MapControllerRoute(
     name: "default",
